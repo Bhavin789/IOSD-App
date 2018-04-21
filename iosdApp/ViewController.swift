326f2101c25389f64035926458291cdf7aed7d74
Bhavin789 <bhavib.789@gmail.com>
2018-04-22 02:08:27 +0530

    Control buttons are added and bug fixes.

--- iosdApp/ViewController.swift
+++ iosdApp/ViewController.swift
@@ -9,52 +9,206 @@
 import UIKit
 import AVFoundation
 
-class VideoPlayerView: UIView {
-    var urlString : String?
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        backgroundColor = UIColor.black
-        self.urlString = "https://firebasestorage.googleapis.com/v0/b/iosdapp-8dd28.appspot.com/o/videoplayback.mp4?alt=media&token=aa5fea98-fc59-4dc7-b2cc-0a1e6a4c480b"
-        if let videoUrl = URL(string: urlString!){
-            let player = AVPlayer(url: videoUrl)
-            let playerLayer = AVPlayerLayer(player: player)
-            self.layer.addSublayer(playerLayer)
-            playerLayer.frame = self.frame
-            
-            player.play()
-            
-        }
-        
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
-
-
 class ViewController: UIViewController {
     
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
-
+    var flag: Int?
     var exercises = [Exercise]()
+    var player : AVPlayer!
+    var playerLayer: AVPlayerLayer!
+    
+    let loginRegisterButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor(red: 58/255, green: 128/255, blue: 188/255, alpha: 1)
+        button.setTitle("Skip", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 6
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 15)
+        button.restorationIdentifier="loginbutton"
+        
+        button.addTarget(self, action: #selector(handleLoginRegister), for: .touchUpInside)
+        return button
+    }()
+    
+    let prevButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor(red: 58/255, green: 128/255, blue: 188/255, alpha: 1)
+        button.setTitle("prev", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 6
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 15)
+        button.addTarget(self, action: #selector(handleLoginRegister), for: .touchUpInside)
+        return button
+    }()
+    
+    let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor(red: 58/255, green: 128/255, blue: 188/255, alpha: 1)
+        button.setTitle("next", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 6
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 15)
+        button.addTarget(self, action: #selector(handleLoginRegister), for: .touchUpInside)
+        return button
+    }()
+    
+    let playButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor(red: 58/255, green: 128/255, blue: 188/255, alpha: 1)
+        button.setTitle("play", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 6
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 15)
+        button.addTarget(self, action: #selector(handleLoginRegister), for: .touchUpInside)
+        return button
+    }()
+    
+    let stopButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor(red: 58/255, green: 128/255, blue: 188/255, alpha: 1)
+        button.setTitle("stop", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 6
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 15)
+        button.addTarget(self, action: #selector(handleLoginRegister), for: .touchUpInside)
+        return button
+    }()
+    
+    let VideoPlayerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.black
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.masksToBounds = true
+        return view
+        
+    }()
+    
+    let controlView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.black
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.masksToBounds = true
+        return view
+        
+    }()
+    
+    let testView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.green
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.masksToBounds = true
+        return view
+        
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.white
         exercises.append(appDelegate.ex1)
         exercises.append(appDelegate.ex2)
-        let videoPlayerFrame = CGRect(x: 0, y: 30, width: view.frame.width, height: view.frame.height - 200)
-        let videoPlayerView = VideoPlayerView(frame: videoPlayerFrame)
+        flag = 0
+        view.addSubview(VideoPlayerView)
+        let tapped = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        let videoPlayerFrame = CGRect(x: 0, y: 120, width: view.frame.width, height: view.frame.height - 320)
+        
+        view.addSubview(loginRegisterButton)
+        view.addSubview(controlView)
+        view.addGestureRecognizer(tapped)
+        view.isUserInteractionEnabled = true
+        
+        controlView.addSubview(playButton)
+        controlView.addSubview(stopButton)
+        controlView.addSubview(prevButton)
+        controlView.addSubview(nextButton)
+        
+        controlView.isHidden = true
+        
+        VideoPlayerView.frame = videoPlayerFrame
+        //let videoPlayerView = VideoPlayerView(frame: videoPlayerFrame)
         //videoPlayerView.urlString = appDelegate.ex1.tutorialUrl
-        view.addSubview(videoPlayerView)
+
         print("HI there")
+        
+        let urlString = "https://firebasestorage.googleapis.com/v0/b/iosdapp-8dd28.appspot.com/o/videoplayback.mp4?alt=media&token=aa5fea98-fc59-4dc7-b2cc-0a1e6a4c480b"
+        if let videoUrl = URL(string: urlString){
+            player = AVPlayer(url: videoUrl)
+            playerLayer = AVPlayerLayer(player: player)
+            playerLayer.videoGravity = .resize
+            VideoPlayerView.layer.addSublayer(playerLayer)
+            //playerLayer.frame = VideoPlayerView.frame
+            player.play()
+        }
+        
+        loginRegisterButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        //loginRegisterButton.bottomAnchor.constraint(equalTo: VideoPlayerView.topAnchor, constant: -12).isActive = true
+        loginRegisterButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 170).isActive = true
+        loginRegisterButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -12).isActive = true
+        loginRegisterButton.heightAnchor.constraint(equalToConstant: 45).isActive = true
+        
+        controlView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
+        controlView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0).isActive = true
+        controlView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        controlView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        
+        print((view.frame.width))
+        prevButton.topAnchor.constraint(equalTo: controlView.topAnchor, constant: 8).isActive = true
+        prevButton.leftAnchor.constraint(equalTo: controlView.leftAnchor, constant: 8).isActive = true
+        prevButton.rightAnchor.constraint(equalTo: controlView.rightAnchor, constant: -(3*((view.frame.width)/4) + 8)).isActive = true
+        prevButton.bottomAnchor.constraint(equalTo: controlView.bottomAnchor, constant: -8).isActive = true
+        
+        playButton.topAnchor.constraint(equalTo: controlView.topAnchor, constant: 8).isActive = true
+        playButton.leftAnchor.constraint(equalTo: prevButton.rightAnchor, constant: 8).isActive = true
+        playButton.rightAnchor.constraint(equalTo: controlView.rightAnchor, constant: -(2*((view.frame.width)/4) + 8)).isActive = true
+        playButton.bottomAnchor.constraint(equalTo: controlView.bottomAnchor, constant: -8).isActive = true
+        
+        stopButton.topAnchor.constraint(equalTo: controlView.topAnchor, constant: 8).isActive = true
+        stopButton.leftAnchor.constraint(equalTo: playButton.rightAnchor, constant: 8).isActive = true
+        stopButton.rightAnchor.constraint(equalTo: controlView.rightAnchor, constant: -(1*((view.frame.width)/4) + 8)).isActive = true
+        stopButton.bottomAnchor.constraint(equalTo: controlView.bottomAnchor, constant: -8).isActive = true
+        
+        nextButton.topAnchor.constraint(equalTo: controlView.topAnchor, constant: 8).isActive = true
+        nextButton.leftAnchor.constraint(equalTo: stopButton.rightAnchor, constant: 8).isActive = true
+        nextButton.rightAnchor.constraint(equalTo: controlView.rightAnchor, constant: -8).isActive = true
+        nextButton.bottomAnchor.constraint(equalTo: controlView.bottomAnchor, constant: -8).isActive = true
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        playerLayer.frame = VideoPlayerView.bounds
+    }
+    
+    @objc func handleTap(_ sender: UITapGestureRecognizer){
+        print("tapped")
+        if(flag == 0){
+            player.pause()
+            controlView.isHidden = false
+            flag = 1
+        }else{
+            player.play()
+            controlView.isHidden = true
+            flag = 0
+        }
+    }
+    
+    @objc func handleLoginRegister(){
+        print("reg")
     }
 
 
